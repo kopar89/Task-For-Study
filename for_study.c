0a33f709cd25df7407e1819b583933825fc68a26
kopar89 <158470217+kopar89@users.noreply.github.com>
2026-02-14 20:07:59 +0300

    Again fix error culc

--- for_study.c
+++ for_study.c
@@ -4,23 +4,37 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <math.h>
+#include <string.h>  
+#include <time.h>
 
-void double32ToMachinecode_file(double num, FILE *fp) {
-    uint32_t bits = *(uint32_t*)&num;
+void float32ToMachinecode_file(float num, FILE *fp) {
+    uint32_t bits;
+    memcpy(&bits, &num, sizeof(bits));  // Безопасное копирование
+    
     for (int i = 31; i >= 0; i--) {
         fprintf(fp, "%d", (bits >> i) & 1);
         if (i % 4 == 0) 
-        fprintf(fp, " "); 
+            fprintf(fp, " "); 
     }
 }
 
 void double64ToMachinecode_file(double num, FILE *fp) {
-    uint64_t bits = *(uint64_t*)&num;
+    uint64_t bits;
+    memcpy(&bits, &num, sizeof(bits));  // Безопасное копирование
+    
     for (int i = 63; i >= 0; i--) {
         fprintf(fp, "%d", (bits >> i) & 1);
         if (i % 4 == 0) 
-        fprintf(fp, " ");
+            fprintf(fp, " ");
     }
+}
+
+float MachinecodeToFloat(uint32_t bits) {
+    return *(float*)&bits;
+}
+
+double MachinecodeToDouble(uint64_t bits) {
+    return *(double*)&bits;
 }
 
 
@@ -105,7 +119,6 @@ void genereta_task(){
         i++;
     }
     fclose(fp);
-    const int size = arr[0];
 
     const char *dir = "Task";
     if (mkdir(dir, 0777) != 0 && errno != EEXIST) {
@@ -113,7 +126,7 @@ void genereta_task(){
         return;
     }
 
-    char variant_filename[size];
+    char variant_filename[256];
     for (int v = 1; v <= arr[0]; v++) {
         sprintf(variant_filename, "%s/variant_%d.md", dir, v);
 
@@ -171,7 +184,7 @@ void solve_task() {
 
     const int size = arr[0];
     for (int v = 1; v <= size; v++) {
-        char filename[size];
+        char filename[256];
         sprintf(filename, "%s/variant_%d.md", dir, v);
 
         FILE *wp = fopen(filename, "w");
@@ -191,15 +204,15 @@ void solve_task() {
             }
 
             double rounded = num;  
-            double error = fabs(num - (double)rounded); 
-
+            double error = (arr[2] == 32) ? fabs(num - (double)(float)num) : 0.0;
             fprintf(wp, "%d.\t%.*f\t", t, arr[5], num);
 
-            if (arr[2] == 32) {
-                double32ToMachinecode_file(num, wp);
-            } else if (arr[2] == 64) {
-                double64ToMachinecode_file(num, wp);
-            }
+        if (arr[2] == 32) {
+        float num32 = (float)num;  // Конвертируем в float
+        float32ToMachinecode_file(num32, wp);
+        } else if (arr[2] == 64) {
+        double64ToMachinecode_file(num, wp);
+        }
             fprintf(wp, "\t%.3e\n", error); 
         }
 
@@ -210,8 +223,8 @@ void solve_task() {
     fclose(fp);
 }
 
-
 int main(){
+    srand(time(NULL));
     printf("\nЭто программа для помощи преподователям в создании учебно-практического материала.\nДля начала работы нужно указать некотурую информацию\n\n");
     input_file_for_study();
     genereta_task();
